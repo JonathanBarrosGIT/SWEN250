b1b61e42dd1629eae08733db06bc3d5a506f3f25
Jonathan Barros <jxc3141@rit.edu>
2015-10-14 13:21:45 -0400

    *** Temperature Converter Part 1 Completed ***

--- TempConvert/chart1.c
+++ TempConvert/chart1.c
@@ -1,1 +1,20 @@
+// Jonathan Correia de Barros
+// Oct 14th 2015
+// Temperature Converter Part 1
 
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	int celsius;
+	int fahrenheit = 0;
+	printf("Fahrenheit -> Celsius\n");
+
+	for(fahrenheit; fahrenheit <= 300 ; fahrenheit += 20)
+	{
+		celsius = (5 * (fahrenheit - 32)) / 9;
+		printf("%d -> %d\n", fahrenheit, celsius);
+	}
+	return 0;
+}
