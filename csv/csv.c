c1dd7a7e66ed14c39cb8b68ba4460a79139608a2
Jonathan Barros <jxc3141@rit.edu>
2015-10-28 23:35:48 -0400

    *** CSV Parser Completed ***
     On branch master
    
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	new file:   ActivityJournal.txt
    	new file:   csv
    	new file:   csv.c
    	new file:   csv.h
    	new file:   expected_print_csv.out
    	new file:   food.csv
    	new file:   print_csv.out
    	new file:   test
    	new file:   unit_tests.c
    	new file:   unit_tests.h
    
     Changes not staged for commit:
       (use "git add/rm <file>..." to update what will be committed)
       (use "git checkout -- <file>..." to discard changes in working directory)
    
    	deleted:    ../DietManager/logItem.rb
    	deleted:    ../LongestLine/fileTest.txt
    	deleted:    ../LongestLine/longest.c

---
+++ csv/csv.c
@@ -0,0 +1,126 @@
+/*
+ *	Jonathan Correia de Barros
+ *	Oct 28th 2015
+ */
+
+/*
+ * Skeleton implementation for the activity to parse and print
+ * CSV formatted files read from standard input.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "csv.h"
+#include "unit_tests.h"
+
+
+/*
+ * Returns true iff the character 'ch' ends a field. That is, ch is end of file,
+ * a comma, or a newline.
+ */
+
+bool is_end_of_field(int ch) {
+	return (ch == ',') || (ch == '\n') || (ch == EOF) ;
+}
+
+/*
+ * Return the minimum of two integers.
+ */
+
+int min(int x, int y) {
+	return x < y ? x : y ;
+}
+
+/*
+ * Read the next field from standard input. Returns the value of getchar() that
+ * stopped (terminated) the field.
+ */
+
+int get_field(f_string field) {
+	int i;
+    int character;
+
+    for (i = 0; !is_end_of_field(character = getchar()); i++)
+    {
+        field[i] = character;
+    }
+    field[i] = '\0';
+
+    return character;
+}
+
+/*
+ * Read in a CSV line. No error checking is done on the number of fields or
+ * the size of any one field.
+ * On return, the fields have been filled in (and properly NUL-terminated), and
+ * nfields is the count of the number of valid fields.
+ * nfields == 0 means end of file was encountered.
+ */
+
+csv_line get_line() {
+	csv_line line ;
+    int i = 0;
+	line.nfields = 0;
+
+    int character = get_field(line.field[i++]);
+
+    // When the first comma is found, we can assume there are already 2 fields
+    if(character == ',')
+    {
+    	line.nfields += 2;
+    }
+
+    while(character != '\n' && character != EOF)
+    {
+    	character = get_field(line.field[i++]);
+
+    	if(character == ',')
+    	{
+    		line.nfields++;
+    	}
+    }
+    return line ;
+}
+
+/*
+ * Print a CSV line, associating the header fields with the
+ * data line fields.
+ * The minimum of the number of fields in the header and the data
+ * determines how many fields are printed.
+ */
+
+void print_csv(csv_line header, csv_line data) {
+	int i ;
+    int limit = min(header.nfields, data.nfields) ;
+
+    for (i = 0; i < limit; i++)
+    {
+    	printf("%s = %s\n", header.field[i], data.field[i]) ;
+    }
+}
+
+/*
+ * Driver - read a CSV line for the header then read and print data lines
+ * until end of file.
+ */
+
+int main( int argc, char *argv[] ) {
+	csv_line header ;
+	csv_line current ;
+
+	// LLK additional if statement to execute unit tests if program
+	// name contains "test".
+	if ( strstr( argv[0], "test" ) )
+		return test() ;
+
+	header = get_line() ;
+	current = get_line() ;
+
+	while ( current.nfields > 0 ) {
+		print_csv(header, current) ;
+		current = get_line() ;
+	}
+
+	return 0 ;
+}
